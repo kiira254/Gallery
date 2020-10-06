772b92760face6192b42b45874569387f9ff060f
kiira254 <nkamotho57@gmail.com>
2020-10-06 16:01:13 +0300

    editor

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
+
+    def __str__(self):
+        return self.first_name
+    class meta:
+        ordering =['name']
+    
+    def save_editor(self):
+        self.save()
