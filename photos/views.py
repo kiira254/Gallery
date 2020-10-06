cf108a26cb153b36178cdd4479aa49ff40573ebc
kiira254 <nkamotho57@gmail.com>
2020-10-06 15:14:35 +0300

    add url

---
+++ photos/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def welcome( request ):
+    return HttpResponse ('welcome to my gallery')
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nelly/Documents/moringa/Python/DJANGO/Gallery/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
