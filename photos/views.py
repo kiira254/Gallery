b326068575d25a17286eb357fd89c9ed26ea29a8
kiira254 <nkamotho57@gmail.com>
2020-10-06 15:22:29 +0300

    add photos of the day

--- photos/views.py
+++ photos/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+import datetime as dt
 
 def welcome( request ):
     return HttpResponse ('welcome to my gallery')
+
+def photos_of_day(request):
+    date = dt.date.today()
+    photos = Article.todays_photos()
+    return render(request, 'all-photos/today-photos.html',{'date': date,"photos":photos})
+
