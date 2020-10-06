7d526f279b6c7ce33ca83ef4f1426eccb55d11fa
kiira254 <nkamotho57@gmail.com>
2020-10-06 16:25:36 +0300

    test

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.test import TestCase
+from .models import Editor,Article,tags
+import datetime as dt
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new tag and saving it
+
+        self.new_categories = categories(name = 'testing')
+        self.new_category.save()
+
+        self.new_image= Image(title = 'Test Article',post = 'This is a random test Post')
+        self.new_image.save()
+
+        self.new_image.categories.add(self.new_categories)
+
+    def tearDown(self):
+        categories.objects.all().delete()
+        Image.objects.all().delete()
+
+    def test_get_photos_today(self):
+        today_photos = Image.todays_photos()
+        self.assertTrue(len(today_photos)>0)
+
+    def test_get_photos_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        photos_by_date = Image.days_photos(date)
+        self.assertTrue(len(photos_by_date) == 0)
