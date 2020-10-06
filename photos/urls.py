72f81607e250ab75b605e4dd03711306242e9b59
kiira254 <nkamotho57@gmail.com>
2020-10-06 16:38:37 +0300

    add search path

--- photos/urls.py
+++ photos/urls.py
@@ -5,5 +5,5 @@ urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url('^today/$',views.photos_of_day,name='photosToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
-    
+    url(r'^search/', views.search_results, name='search_results')
 ]
