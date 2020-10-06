b0de8fe666acd2912cfcd6b1d7bdab6131485f0f
kiira254 <nkamotho57@gmail.com>
2020-10-06 15:39:13 +0300

    add installation permit

--- photos/views.py
+++ photos/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import datetime as dt
 
 def welcome( request ):
