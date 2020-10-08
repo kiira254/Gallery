ebdee979fd2b377c5560b96a1a01a317377a57c2
kiira254 <nkamotho57@gmail.com>
2020-10-08 10:51:53 +0300

    add picture as the main page url

--- photos/models.py
+++ photos/models.py
@@ -28,8 +28,8 @@ class Image(models.Model):
     Image = models.ImageField(upload_to = 'image/')
     Name = models.CharField(max_length =60)
     Description = models.TextField()
-    Categories= models.ManyToManyField(Categories)
-    # Location = models.ForeignKey(User,on_delete=models.CASCADE)
+    # Categories= models.ManyToManyField(Categories)
+    Location = models.ForeignKey(User,on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -49,4 +49,8 @@ class Image(models.Model):
     def search_by_categories(cls,search_term):
         photos = cls.objects.filter(categories__icontains=search_term)
         return photos
+
+    @classmethod
+    def save_image (cls):
+        photos=cls.objects.filter(Categories=Categories)
 
